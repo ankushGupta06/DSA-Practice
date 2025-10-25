5f449da25f45cb66b60ee9298a84677ef412ec03
Ankush Gupta <iamankushgupta68@gmail.com>
2025-10-25 11:56:38 +0530

    BFS for disconnected node and DFS for connected nodes

--- Graphs/graph_traversal.py
+++ Graphs/graph_traversal.py
@@ -1,5 +1,29 @@
 from collections import deque
 
+# This BFS is used to traverse the entire graph, including disconnected components.
+def BFS_all(n, adj):
+    visited = [0]*(n+1)
+    ans = []
+
+    def traversal(node, visited, graph):
+        queue = deque()
+        queue.append(node)
+        visited[node] = 1
+
+        while queue:
+            current = queue.popleft()
+            ans.append(current)
+            for neighbor in graph[current]:
+                if not visited[neighbor]:
+                    visited[neighbor] = 1
+                    queue.append(neighbor)
+
+    for i in range(1,n+1):
+        if not visited[i]:
+            traversal(i, visited, adj)
+    return ans
+
+# This BFS is used to traverse a graph from a given starting node.
 def BFS(n,adj, starting_node):
     ans = []
     queue = deque()
@@ -16,6 +40,7 @@ def BFS(n,adj, starting_node):
                 queue.append(neighbor)
     return ans
 
+# This DFS is used to traverse the entire graph, including disconnected components.
 def DFS(n, adj):
     visited = [0]*(n+1)
     ans = []
@@ -32,6 +57,16 @@ def DFS(n, adj):
             traversal(i, visited, adj)
     return ans
 
+
+#This DFS is used to traverse a graph from a given starting node and only for the connected nodes.
+def newDFS(node, result, visited, graph):
+    visited[node] = 1
+    result.append(node)
+    for n in graph[node]:
+        if visited[n] != 1:
+            newDFS(n, result, visited, graph)
+
+
 n = 9
 adjacency_list = [
     [],
@@ -45,6 +80,9 @@ adjacency_list = [
     [1,7,9],
     [8]
 ]
+print("BFS Traversal of the entire graph:")
+bfs_all_result = BFS_all(n, adjacency_list)
+print(bfs_all_result) # Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 print("BFS Traversal starting from node 3:")
 bfs_result = BFS(n, adjacency_list, 3)
@@ -53,3 +91,9 @@ print(bfs_result)  # Output: [3, 2, 4, 1, 5, 6, 7, 8, 9]
 print("\nDFS Traversal of the graph:")
 dfs_result = DFS(n, adjacency_list)
 print(dfs_result)
+
+print("\nNew DFS Traversal starting from node 3:")
+visited_nodes = [0]*(n+1)
+new_dfs_result = []
+newDFS(3, new_dfs_result, visited_nodes, adjacency_list)
+print(new_dfs_result)  # Output: [3, 2, 1, 8, 7, 6, 5, 4, 9]
